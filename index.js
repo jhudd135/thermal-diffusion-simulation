bbf6370ea985bc250ccea3477c52b1eb3d7fe232
jhudd135 <jhudd135@gmail.com>
2024-04-10 23:31:06 -0400

    functional demo complete

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import { canvasInit, Canvas} from "./canvas.js";
 import { createSubject, Subject } from "./simulation.js";
+import { controlsInit } from "./controls.js";
 
 export function init() {
     canvasInit();
-    Canvas.CANVAS.borderCanvas();
-
-    const s = createSubject(30, 50, 7);
-    s.startSim();
-
-    
+    controlsInit();
+    Canvas.CANVAS.borderCanvas();    
 }
